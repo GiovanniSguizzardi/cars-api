d4a554cce192a4f30ab58c59abac0b21173789bd
giovannisguizzardi <giovanni.sguiconde@gmail.com>
2026-08-24 20:43:27 -0300

    feat: @repository

--- src/main/java/br/com/fiap/cars_api/repository/RenaultRepository.java
+++ src/main/java/br/com/fiap/cars_api/repository/RenaultRepository.java
@@ -4,6 +4,7 @@ import br.com.fiap.cars_api.model.Renault;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+@Repository
 public interface RenaultRepository extends JpaRepository<Renault, Long>{
 
 }
